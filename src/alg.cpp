7a056dece91e5616563b3af52ac2584e043fddb2
abdelaziztt <99320708+abdelaziztt@users.noreply.github.com>
2022-03-27 00:00:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include "alg.h"
+using namespace std;
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    cout << gcd(a,b) << endl;
+    return 0;
 }
